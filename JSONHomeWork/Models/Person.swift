a7cf6082f5b54d978b22a1b9479c1d36556b7729
Gun <ggunin1992@gmail.com>
2023-03-21 21:57:46 +0300

    Initial Commit

---
+++ JSONHomeWork/Models/Person.swift
@@ -0,0 +1,31 @@
+//
+//  Person.swift
+//  JSONHomeWork
+//
+//  Created by 1234 on 21.03.2023.
+//
+
+import Foundation
+
+struct Person: Decodable {
+    let name: [Name]?
+    let gender: String
+    let location: [Location]
+    let email: String
+    let picture: [Picture]
+}
+
+struct Name: Decodable {
+    let first: String
+    let last: String
+}
+
+struct Location: Decodable {
+    let country: String
+    let state: String
+    let city: String
+}
+
+struct Picture: Decodable {
+    let large: URL
+}
---
+++ JSONHomeWork/ViewControllers/MainViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  JSONHomeWork
+//
+//  Created by 1234 on 21.03.2023.
+//
+
+import UIKit
+
+
+final class MainViewController: UIViewController {
+    
+    enum Link: String {
+        case personAPI = "https://randomuser.me/api/"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getPerson()
+    }
+    
+    let personURL = "https://randomuser.me/api/"
+    
+}
+    
+    extension MainViewController {
+        
+        private func getPerson() {
+            guard let url = URL(string: personURL) else { return }
+            
+            URLSession.shared.dataTask(with: url) { data, _, error in
+                guard let data else {
+                    print(error?.localizedDescription ?? "No error description")
+                    return
+                }
+                let decoder = JSONDecoder()
+                
+                do {
+                    let personInfo = try decoder.decode(Person.self, from: data)
+                    print(personInfo)
+                } catch let error {
+                    print(error)
+                }
+            }.resume()
+            
+        }
+    }
